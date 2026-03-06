02372af1881eff06bae6075d1f2585c0fdee7412
Egor <EGS-76@yandex.ru>
2026-03-06 23:00:50 +0300

    avl splay

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -10,8 +10,74 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
     
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        newNode.Height = 1;
+        var current = newNode.Parent;
+        while (current != null)
+        {
+            var parent = current.Parent;
+            Balance(current);
+            current = parent;
+        }
     }
 
-    
+    protected override void OnNodeRemoved(AvlNode<TKey, TValue>? parent, AvlNode<TKey, TValue>? child)
+    {
+        var current = parent;
+        while (current != null)
+        {
+            var nextParent = current.Parent;
+            Balance(current);
+            current = nextParent;
+        }
+    }
+
+    private int GetHeight(AvlNode<TKey, TValue>? node)
+    {
+        return node?.Height ?? 0;
+    }
+
+    private int GetBalanceFactor(AvlNode<TKey, TValue>? node)
+    {
+        if (node == null) return 0;
+        return GetHeight(node.Right) - GetHeight(node.Left);
+    }
+
+    private void UpdateHeight(AvlNode<TKey, TValue>? node)
+    {
+        if (node == null) return;
+        node.Height = Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
+    }
+
+    private void Balance(AvlNode<TKey, TValue> node)
+    {
+        UpdateHeight(node);
+        int balanceFactor = GetBalanceFactor(node);
+
+        if (balanceFactor == 2)
+        {
+            if (GetBalanceFactor(node.Right) < 0)
+            {
+                var right = node.Right;
+                RotateRight(right!);
+                UpdateHeight(right);
+                UpdateHeight(node.Right);
+            }
+            RotateLeft(node);
+            UpdateHeight(node);
+            UpdateHeight(node.Parent);
+        }
+        else if (balanceFactor == -2)
+        {
+            if (GetBalanceFactor(node.Left) > 0)
+            {
+                var left = node.Left;
+                RotateLeft(left!);
+                UpdateHeight(left);
+                UpdateHeight(node.Left);
+            }
+            RotateRight(node);
+            UpdateHeight(node);
+            UpdateHeight(node.Parent);
+        }
+    }
 }
--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -11,17 +11,136 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (parent != null)
+        {
+            Splay(parent);
+        }
     }
     
+    public override bool ContainsKey(TKey key)
+    {
+        return FindNodeAndSplay(key) != null;
+    }
+
+    public override bool Remove(TKey key)
+    {
+        if (Root == null) return false;
+
+        var node = FindNodeAndSplay(key);
+        if (node == null || Comparer.Compare(node.Key, key) != 0)
+        {
+            return false;
+        }
+
+        if (node.Left == null)
+        {
+            Transplant(node, node.Right);
+        }
+        else if (node.Right == null)
+        {
+            Transplant(node, node.Left);
+        }
+        else
+        {
+            var y = node.Right;
+            while (y.Left != null)
+            {
+                y = y.Left;
+            }
+
+            if (y.Parent != node)
+            {
+                Transplant(y, y.Right);
+                y.Right = node.Right;
+                y.Right.Parent = y;
+            }
+
+            Transplant(node, y);
+            y.Left = node.Left;
+            y.Left.Parent = y;
+        }
+
+        Count--;
+        return true;
+    }
+
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        var node = FindNodeAndSplay(key);
+        if (node != null)
+        {
+            value = node.Value;
+            return true;
+        }
+        value = default;
+        return false;
     }
     
+    private BstNode<TKey, TValue>? FindNodeAndSplay(TKey key)
+    {
+        var node = Root;
+        BstNode<TKey, TValue>? parent = null;
+        
+        while (node != null)
+        {
+            int cmp = Comparer.Compare(key, node.Key);
+            if (cmp == 0)
+            {
+                Splay(node);
+                return node;
+            }
+            parent = node;
+            node = cmp < 0 ? node.Left : node.Right;
+        }
+
+        if (parent != null)
+        {
+            Splay(parent);
+        }
+
+        return null;
+    }
+
+    private void Splay(BstNode<TKey, TValue> x)
+    {
+        while (x.Parent != null)
+        {
+            if (x.Parent.Parent == null)
+            {
+                if (x == x.Parent.Left)
+                {
+                    RotateRight(x.Parent);
+                }
+                else
+                {
+                    RotateLeft(x.Parent);
+                }
+            }
+            else if (x == x.Parent.Left && x.Parent == x.Parent.Parent.Left)
+            {
+                RotateRight(x.Parent.Parent);
+                RotateRight(x.Parent);
+            }
+            else if (x == x.Parent.Right && x.Parent == x.Parent.Parent.Right)
+            {
+                RotateLeft(x.Parent.Parent);
+                RotateLeft(x.Parent);
+            }
+            else if (x == x.Parent.Right && x.Parent == x.Parent.Parent.Left)
+            {
+                RotateLeft(x.Parent);
+                RotateRight(x.Parent);
+            }
+            else
+            {
+                RotateRight(x.Parent);
+                RotateLeft(x.Parent);
+            }
+        }
+    }
 }
